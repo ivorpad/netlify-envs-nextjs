0fd40303850daeadab7fcb8f2416666eba60308c
ivorpad <ivorjpc@gmail.com>
2022-03-14 23:33:11 +0100

    test

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import getConfig from 'next/config'
+
+const { serverRuntimeConfig } = getConfig();
 
 type Data = {
-  name: string
-}
+  name: string;
+  env?: string
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: "John Doe", env: serverRuntimeConfig.API_KEY || "NOT_FOUND" });
 }
