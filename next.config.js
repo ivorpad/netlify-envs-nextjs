db56de9424ad271c1d6007799a6fa6eea1486df9
ivorpad <ivorjpc@gmail.com>
2022-03-15 00:12:46 +0100

    test

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   serverRuntimeConfig: {
     API_KEY: process.env.API_KEY,
-  }
+    CONTEXT: process.env.CONTEXT,
+  },
 };
 
 module.exports = nextConfig;
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -4,14 +4,26 @@ import getConfig from 'next/config'
 
 const { serverRuntimeConfig } = getConfig();
 
+// export const SELF_URL =
+//   process.env.SELF_URL ||
+//   (process.env.CONTEXT === "production"
+//     ? process.env.URL
+//     : process.env.DEPLOY_PRIME_URL);
+
+
 type Data = {
   name: string;
-  env?: string
+  key?: string;
+  context: string;
 };
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: "John Doe", env: serverRuntimeConfig.API_KEY || "NOT_FOUND" });
+  res.status(200).json({
+    name: "John Doe",
+    key: serverRuntimeConfig.API_KEY || "NOT_FOUND",
+    context: serverRuntimeConfig.CONTEXT || "NOT_FOUND",
+  });
 }
