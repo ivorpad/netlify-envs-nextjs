46376674fa9cd3c87581fd885beb8a808be4e8e4
ivorpad <ivorjpc@gmail.com>
2022-03-14 22:42:53 +0100

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  publicRuntimeConfig: {
+    API_KEY: process.env.API_KEY,
+  },
+  serverRuntimeConfig: {
+    API_KEY: process.env.API_KEY,
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
