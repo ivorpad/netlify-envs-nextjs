ea1519bd7201e647060d744042a27119f45baf87
ivorpad <ivorjpc@gmail.com>
2022-03-15 16:10:35 +0100

    testing

--- next.config.js
+++ next.config.js
@@ -6,10 +6,11 @@ const nextConfig = {
     URL: process.env.URL,
     DEPLOY_PRIME_URL: process.env.DEPLOY_PRIME_URL,
     CONTEXT: process.env.CONTEXT,
+    // API_KEY: process.env.API_KEY,
   },
-  serverRuntimeConfig: {
-    API_KEY: process.env.API_KEY,
-  },
+  // serverRuntimeConfig: {
+  //   API_KEY: process.env.API_KEY,
+  // },
 };
 
 module.exports = nextConfig;
