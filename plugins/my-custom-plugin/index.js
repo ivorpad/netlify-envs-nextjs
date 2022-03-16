3204157e971ab84d064e743162d5d9309d01265f
ivorpad <ivorjpc@gmail.com>
2022-03-16 17:47:08 +0100

    a

--- plugins/my-custom-plugin/index.js
+++ plugins/my-custom-plugin/index.js
@@ -1,14 +1,7 @@
 require("dotenv").config();
 
 module.exports = {
-  onPreBuild: ({ constants, inputs, netlifyConfig, ...rest }) => {
-    // console.log(
-    //   "------- netlifyConfig",
-    //   JSON.stringify(netlifyConfig, null, 2)
-    // );
-    // console.log("------- REST", JSON.stringify(rest, null, 2));
-    // console.log("------- constants", constants);
-
-    // netlifyConfig.build.environment.NEXT_PUBLIC_API_KEY = process.env.ANOTHER;
+  onPreBuild: async ({ constants, inputs, netlifyConfig, utils }) => {
+     await run.command("ls");
   },
 };
