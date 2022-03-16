7bab6f7e925abb4714e9e28aa82d596653716968
ivorpad <ivorjpc@gmail.com>
2022-03-16 17:51:44 +0100

    a

--- plugins/my-custom-plugin/index.js
+++ plugins/my-custom-plugin/index.js
@@ -3,10 +3,10 @@ require("dotenv").config();
 module.exports = {
   onPreBuild: async ({ constants, inputs, netlifyConfig, utils: { run } }) => {
     console.log('PREBUILD');
-    await run.command("ls");
+    await run.command("ls -a");
   },
   onPostBuild: async ({ constants, inputs, netlifyConfig, utils: { run } }) => {
     console.log('POST BUILD');
-    await run.command("ls");
+    await run.command("ls -a");
   },
 };
