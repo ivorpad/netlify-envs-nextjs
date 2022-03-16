3082758d3bba2c7a6083087fc8aa271879b257f2
ivorpad <ivorjpc@gmail.com>
2022-03-16 17:48:03 +0100

    a

--- plugins/my-custom-plugin/index.js
+++ plugins/my-custom-plugin/index.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 
 module.exports = {
-  onPreBuild: async ({ constants, inputs, netlifyConfig, utils }) => {
+  onPreBuild: async ({ constants, inputs, netlifyConfig, utils: { run } }) => {
+      console.log('COMMENTED OUT ON PREBUILD');
      await run.command("ls");
   },
 };
